2e62cc8f02d68d0ccf82ba7583a38b3937aaa14a
Darren Reid <darren@reidmail.org>
2021-04-27 18:53:48 +1000

    Header socials. Alignment fixes. Font awesome added.

--- js/custom.js
+++ js/custom.js
@@ -73,7 +73,7 @@ if (matchMedia('(min-width: 992px), (max-width: 767px)').matches) {
         var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
         if (top > 1820) {
             $('.sticky-navigation').stop().animate({'top': '0'}).css({'position': 'fixed'});
-            $('.floating-navigation').stop().animate({'top': '-60'});
+            $('.floating-navigation').stop().animate({'top': '-120'});
         }
         else {
             $('.sticky-navigation').stop().animate({'top': '60'}).css({"position": 'relative'});
@@ -87,7 +87,7 @@ if (matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
         var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
         if (top > 1750) {
             $('.sticky-navigation').stop().animate({'top': '0'}).css({'position': 'fixed'});
-            $('.floating-navigation').stop().animate({'top': '-120'});
+            $('.floating-navigation').stop().animate({'top': '-180'});
         }
         else {
             $('.sticky-navigation').stop().animate({'top': '90'}).css({"position": 'relative'});
