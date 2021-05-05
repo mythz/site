7c25ef29a431dfaf30e90fe72def20a16a99f561
Demis Bellot <demis.bellot@gmail.com>
2021-05-06 02:25:19 +0800

    Fix download page + combine pages under single _pages directory

--- js/custom.js
+++ js/custom.js
@@ -224,6 +224,26 @@ $(window).stellar({
 
 
 /* =================================
+===  Nuget copy                    ====
+=================================== */
+function copy(e) {
+    let $el = document.createElement("input");
+    let $parent = e.parentElement.parentElement;
+    let $lbl = $parent.firstElementChild;
+    $el.setAttribute("value", $lbl.innerText);
+    document.body.appendChild($el);
+    $el.select();
+    document.execCommand("copy");
+    document.body.removeChild($el);
+    let $copyText = $parent.parentElement.parentElement.querySelector('.copy-text');
+    $copyText.innerHTML = '<label>copied!</label>';
+    setTimeout(function () {
+        $copyText.innerHTML = '';
+    }, 5000);
+}
+
+
+/* =================================
 ===  Bootstrap Internet Explorer 10 in Windows 8 and Windows Phone 8 FIX
 =================================== */
 if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
