37239976a53a0412af2e33dc684310ce1c1979b8
Demis Bellot <demis.bellot@gmail.com>
2021-07-03 11:58:53 +0800

    Add Why ServiceStack proj for assets used in Intro video

---
+++ projects/WhyServiceStack/MyServices.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using ServiceStack;
+
+namespace WhyServiceStack
+{
+    class Refs
+    {
+        Refs()
+        {
+            new MyServices().Any(new Hello());
+        }
+    }
+
+
+    [Route("/hello/{Name}")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse 
+            {
+                Result = $"Hi, {request.Name}!"
+            };
+        }
+    }
+
+    class Tests
+    {
+        public void Can_Call_Hello_Service_Url()
+        {
+            var client = new JsonServiceClient("https://localhost:5001");
+            HelloResponse response = client.Get<HelloResponse>("/hello?Name=World");
+            Console.WriteLine(response.Result); //Hi, World!
+        }
+
+        public void Can_Call_Hello_Service_Dti()
+        {
+            var client = new JsonServiceClient("https://localhost:5001");
+            HelloResponse response = client.Get(new Hello { Name = "World" });
+            Console.WriteLine(response.Result); //Hi, World!
+        }
+    }
+
+}
---
+++ projects/WhyServiceStack/dtos.ts
@@ -0,0 +1,56 @@
+﻿// ReSharper disable VariableCanBeMadeConst
+// ReSharper disable StringLiteralWrongQuotes
+
+/* Options:
+Date: 2021-07-02 14:28:17
+Version: 5.105
+Tip: To override a DTO option, remove "//" prefix before updating
+BaseUrl: https://web.web-templates.io
+
+//GlobalNamespace: 
+//MakePropertiesOptional: False
+//AddServiceStackTypes: True
+//AddResponseStatus: False
+//AddImplicitVersion: 
+//AddDescriptionAsComments: True
+//IncludeTypes: 
+//ExcludeTypes: 
+//DefaultImports: 
+*/
+
+
+export interface IReturn<T> {
+    createResponse(): T;
+}
+
+export interface IReturnVoid {
+    createResponse(): void;
+}
+
+export class HelloResponse {
+    public result: string;
+
+    public constructor(init?: Partial<HelloResponse>) { (Object as any).assign(this, init); }
+}
+
+// @Route("/hello/{Name}")
+export class Hello implements IReturn<HelloResponse>
+{
+    public name: string;
+
+    public constructor(init?: Partial<Hello>) { (Object as any).assign(this, init); }
+    public createResponse() { return new HelloResponse(); }
+    public getTypeName() { return 'Hello'; }
+}
+
+class JsonServiceClient { constructor(s: string) { } get(a:any): Promise<string> { return; } }
+
+import { JsonServiceClient } from '@servicestack/client';
+
+async function f() {
+let a = '@servicestack/client';
+
+let client = new JsonServiceClient('https://web.web-templates.io');
+let response = await client.get(new Hello({ name: 'World' }));
+
+}
